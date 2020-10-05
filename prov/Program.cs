db68ee4b6e48001323abd6edcc4cb8e8638b0c42
Aironas <6zouchee9@gmail.com>
2020-10-05 09:36:12 +0200

    Master Commit

---
+++ prov/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace prov
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int ship = 7;
+
+            string ans = "";
+
+            int check = 0;
+
+            Console.WriteLine("Välkommen till bootleg battleships");
+
+            Console.WriteLine("Tryck enter för att fortsätta");
+            Console.ReadLine();
+
+            Console.WriteLine("Skriv ett tal mellan 1-10");
+
+            ans = Console.ReadLine();
+
+            try
+            {
+                check = int.Parse(ans);
+                while (ship != int.Parse(ans))
+                {
+                    Console.WriteLine("du missade");
+                    ans = Console.ReadLine();
+                }
+
+
+
+                if (ship == int.Parse(ans))
+                {
+                    Console.WriteLine("Grattis du fick rätt");
+
+                    Console.ReadLine();
+
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Du skrev inte ett nummer. nu stänger jag ner skiten >:(");
+                Console.ReadLine();
+
+            }
+
+
+
+
+
+
+        }
+
+
+    }
+}
+
